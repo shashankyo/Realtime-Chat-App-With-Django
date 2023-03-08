65e17247555a6108b7be47d302058b5e6ddf6be3
shashankyo <114981861+shashankyo@users.noreply.github.com>
2023-03-08 10:45:56 +0530

    secrete kkey removed

--- djangochat/djangochat/settings.py
+++ djangochat/djangochat/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0z4a_$ww=lwe-5z2sv4r1_l_n+e(ucr(uciojc$@qsgmt6h0@0'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
